f3440e5bf3fe982c6adda43367cc27e3c5f81c50
Scott Gress <scottmgress@gmail.com>
2017-10-02 14:58:44 -0500

    Add whitelist option, and make all options camelCased

--- README.md
+++ README.md
@@ -20,14 +20,15 @@ By default, configuration lives in `sails.config['maintenance-mode']`.  The conf
 
 Parameter      | Type                | Default | Details
 -------------- | ------------------- | ------- | :---------------------------------:
-env_var        | ((string)) | `MAINTENANCE_MODE` | Environment variable to check 
+envVar        | ((string)) | `MAINTENANCE_MODE` | Environment variable to check
 view | ((string)) | _none_ | View to display in maintenance mode.  If not provided, the hook will use the `views/maintenance.ejs` view if your app has one.  Otherwise, it will show a default maintenance page.  Set to `false` to display text or JSON instead of a view (see `text_message` and `json_message` options)
 viewLocals | ((dictionary)) | `{}` | Locals and options to use with your maintenance view.  You can use this to change up the message per environment, or to provide a different layout.
+whitelistUrls | ((array)) | _none_ | Array of strings or regular expressions to match a requested URL against.  If the URL matches anything in the whitelist, it will bypass maintenance mode and be allowed to proceed.  Useful for keeping certain sections of a site (like a contact page or FAQ) live.
 status | ((number)) | 503 (service unavailable) | Status code to respond with in maintenance mode.
-json_message | ((json)) | _none_ | If `view` is set to `false` and `json_message` is provided, the [`res.json`](http://sailsjs.org/documentation/reference/response-res/res-json) method will be used with that value for the response in maintenance mode.
-text_message | ((string)) | _none_ | If `view` is set to `false` and `text_message` is provided (and `json_message` is _not_ provided), the [`res.send`](http://sailsjs.org/documentation/reference/response-res/res-send) method will be used with that value for the response in maintenance mode.
-backdoor_on_param    | ((string)) | _none_ | Parameter which can be used to unlock maintenance mode for a single session.  Use this to test updates to the site while the rest of the world still sees the maintenance page.
-backdoor_off_param    | ((string)) | _none_ | Parameter which can be used to re-instate maintenance mode for a session after the `backdoor_on_param` has been used.
+jsonMessage | ((json)) | _none_ | If `view` is set to `false` and `json_message` is provided, the [`res.json`](http://sailsjs.org/documentation/reference/response-res/res-json) method will be used with that value for the response in maintenance mode.
+textMessage | ((string)) | _none_ | If `view` is set to `false` and `text_message` is provided (and `json_message` is _not_ provided), the [`res.send`](http://sailsjs.org/documentation/reference/response-res/res-send) method will be used with that value for the response in maintenance mode.
+backdoorOnParam    | ((string)) | _none_ | Parameter which can be used to unlock maintenance mode for a single session.  Use this to test updates to the site while the rest of the world still sees the maintenance page.
+backdoorOffParam    | ((string)) | _none_ | Parameter which can be used to re-instate maintenance mode for a session after the `backdoor_on_param` has been used.
 
 #### Example
 
@@ -35,11 +36,11 @@ backdoor_off_param    | ((string)) | _none_ | Parameter which can be used to re-
 // [your-sails-app]/config/env/production.js
 module.exports = {
   'maintenance-mode': {
-    env_var: 'MAINTENANCE',
+    envVar: 'MAINTENANCE',
     view: false,
-    text_message: "Site currently under maintenance, please try again later."
-    backdoor_on_param: 'open_sesame',
-    backdoor_off_param: 'close_sesame',
+    textMessage: "Site currently under maintenance, please try again later."
+    backdoorOnParam: 'open_sesame',
+    backdoorOffParam: 'close_sesame',
     status: 200
   }
 };
--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+var _ = require('@sailshq/lodash');
+
 module.exports = function(sails) {
 
   return {
 
     defaults: {
       __configKey__: {
-        env_var: "MAINTENANCE_MODE",
-        text_message: "Site currently under maintenance.",
+        'envVar': 'MAINTENANCE_MODE',
+        'textMessage': 'Site currently under maintenance.',
+        'whitelistedUrls': [],
         status: 503
       }
     },
 
     configure: function() {
 
+      var self = this;
+
+      // Allow deprecated snake_case config for now.
+      _.each(['env_var','whitelist_urls','json_message','text_message','backdoor_on_param','backdoor_off_param'], function(key) {
+        if (!_.isUndefined(sails.config[self.configKey][key]) && _.isUndefined(sails.config[self.configKey][_.camelCase(key)])) {
+          sails.config[self.configKey][_.camelCase(key)] = sails.config[self.configKey][key];
+          sails.log.warn('The `' + key + '` configuration key in sails-hook-maintenance-mode is deprecated.  Please use `' + _.camelCase(key) + '` instead.');
+        }
+      });
+
       // If no view is configured, try to default to views/maintenance.ejs if it exists
-      if ('undefined' == typeof sails.config[this.configKey].view) {
+      if ('undefined' === typeof sails.config[this.configKey].view) {
         try {
           if (require('fs').existsSync(require('path').resolve(sails.config.appPath, 'views', 'maintenance.ejs'))) {
             sails.config[this.configKey].view = 'maintenance';
@@ -27,8 +40,8 @@ module.exports = function(sails) {
       var self = this;
 
       // Determine if maintenance mode is enabled
-      var inMaintenanceMode = process.env[sails.config[self.configKey].env_var];
-      if (inMaintenanceMode == 'true' || inMaintenanceMode === true) {
+      var inMaintenanceMode = process.env[sails.config[self.configKey].envVar];
+      if (inMaintenanceMode === 'true' || inMaintenanceMode === true) {
 
         // Bind a route that will run before all user and blueprint routes.
         // NOTE -- this will not necessarily run before all other HOOK routes.
@@ -37,21 +50,28 @@ module.exports = function(sails) {
           // Make this a wildcard route that will match any URL
           sails.router.bind('/*', function(req, res, next) {
 
+            // If the URL matches one of the whitelisted URLs...
+            if (_.isArray(sails.config[self.configKey].whitelistUrls) && _.find(sails.config[self.configKey].whitelistUrls, function(whitelistedUrl) {
+              return req.url.match(whitelistedUrl);
+            })) {
+              return next();
+            }
+
             // If a backdoor was provided for maintenance mode...
-            if (sails.config[self.configKey].backdoor_on_param) {
+            if (sails.config[self.configKey].backdoorOnParam) {
 
               // If the "close backdoor" param was used, then close the back door so that requests
               // will once again be met with the maintenance mode page.
-              if (sails.config[self.configKey].backdoor_off_param && req.param(sails.config[self.configKey].backdoor_off_param)) {
+              if (sails.config[self.configKey].backdoorOffParam && req.param(sails.config[self.configKey].backdoorOffParam)) {
                 req.session.unlockMaintenanceMode = false;
               }
 
               // If the "open backdoor" param was used now or previously, continue serving the request
               // instead of showing maintenance mode.
-              if (req.param(sails.config[self.configKey].backdoor_on_param) || req.session.unlockMaintenanceMode) {
+              if (req.param(sails.config[self.configKey].backdoorOnParam) || req.session.unlockMaintenanceMode) {
                 req.session.unlockMaintenanceMode = true;
                 return next();
-              }              
+              }
             }
 
             // Set the response status
@@ -59,15 +79,15 @@ module.exports = function(sails) {
 
             // If the "view" config was explicitly set to false, just show a JSON or text message.
             if (sails.config[self.configKey].view === false || res.wantsJSON) {
-              if (sails.config[self.configKey].json_message) {
-                return res.json(sails.config[self.configKey].json_message);
+              if (sails.config[self.configKey].jsonMessage) {
+                return res.json(sails.config[self.configKey].jsonMessage);
               }
-              return res.send(sails.config[self.configKey].text_message);              
+              return res.send(sails.config[self.configKey].textMessage);
             }
             // If "view" config was set, then serve that view
             else if (sails.config[self.configKey].view) {
               return res.view(sails.config[self.configKey].view, sails.config[self.configKey].viewLocals || {});
-            } 
+            }
             // Otherwise if "view" config was left completely blank, serve our default maintenance page
             else {
               return res.view(require('path').resolve(__dirname, 'maintenance.ejs'), {layout: false});
